828199d13cdf482e7cbecf617ab43de5a207b782
khaledbadawy1994 <166828748+khaledbadawy1994@users.noreply.github.com>
2024-05-05 12:40:24 -0700

    Update Ad-Click.py

--- Ad-Click.py
+++ Ad-Click.py
@@ -219,12 +219,12 @@ print("Classification Report:")
 print(classification_report(y_test, y_pred_test))
 print('--------------------------------------------------------------')
     # Confusion matrix
-    plt.figure(figsize=(5, 4))
-    sns.heatmap(confusion_matrix(y_test, y_pred_test), annot=True, cmap=color)
-    plt.xlabel('Predicted')
-    plt.ylabel('Actual')
-    plt.title('Confusion Matrix')
-    plt.show()
+plt.figure(figsize=(5, 4))
+sns.heatmap(confusion_matrix(y_test, y_pred_test), annot=True, cmap=color)
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.title('Confusion Matrix')
+plt.show()
 
 def model_tunning(model,X_train,y_train,parameters):
     '''
